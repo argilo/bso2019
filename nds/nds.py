1792dd47cf3a7e874f7483480aaed2b8142444d0
Clayton Smith <argilo@gmail.com>
2019-10-27 15:08:25 -0400

    Add docstrings

--- nds/nds.py
+++ nds/nds.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+# S-boxes
+S0 = [1, 8, 12, 10, 4, 3, 13, 5, 14, 0, 2, 6, 15, 9, 7, 11]
+S1 = [15, 9, 8, 14, 2, 7, 6, 13, 4, 10, 11, 12, 0, 3, 1, 5]
+
+# permutation (P-box)
 P = [
     58, 53, 14, 26, 8, 57, 34, 15, 23, 62, 24, 31, 59, 2, 3, 43,
     20, 54, 56, 32, 55, 41, 36, 35, 9, 47, 33, 6, 29, 12, 4, 51,
@@ -7,11 +12,9 @@ P = [
     1, 10, 42, 25, 48, 49, 40, 0, 18, 50, 27, 44, 22, 39, 38, 21
 ]
 
-S0 = [1, 8, 12, 10, 4, 3, 13, 5, 14, 0, 2, 6, 15, 9, 7, 11]
-S1 = [15, 9, 8, 14, 2, 7, 6, 13, 4, 10, 11, 12, 0, 3, 1, 5]
-
 
 def f(input, key):
+    """the Feistel round function"""
     highbits = 0
     for x in range(8):
         highbits |= (input[x] & 0x80) >> x
@@ -33,12 +36,14 @@ def f(input, key):
 
 
 def cipher_block(input, key):
+    """encrypt or decrypt a single block"""
     for x in range(16):
         input = input[8:16] + bytes([a ^ b for a, b in zip(input[0:8], f(input[8:16], key))])
     return input[8:16] + input[0:8]
 
 
 def cipher(input, key):
+    """encrypt or decrypt multiple blocks in ECB mode"""
     output = b""
     for x in range(0, len(input), 16):
         output += cipher_block(input[x:x+16], key)
