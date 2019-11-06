ac4e712d6dfc7b95a89d76310ba28cea8686f773
Clayton Smith <argilo@gmail.com>
2019-11-05 20:14:23 -0500

    Floyd's algorithm

--- hash/collide.py
+++ hash/collide.py
@@ -2,6 +2,32 @@
 
 import hashlib
 
+HASH_BYTES = 6
+
+def hash(b):
+    return hashlib.sha256(b).digest()[:HASH_BYTES]
+
+x0 = bytes([0] * HASH_BYTES)
+
+tortoise = hash(x0)
+hare = hash(hash(x0))
+
+while tortoise != hare:
+    tortoise = hash(tortoise)
+    hare = hash(hash(hare))
+
+tortoise = x0
+while tortoise != hare:
+    in1, in2 = tortoise, hare
+    tortoise = hash(tortoise)
+    hare = hash(hare)
+
+print(in1, in2)
+print(hashlib.sha256(in1).hexdigest())
+print(hashlib.sha256(in2).hexdigest())
+
+quit()
+
 seen = {}
 
 n = 0
