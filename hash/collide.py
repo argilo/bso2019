23668b5e85ef13359b768a94ab8e3c9f895f586d
Clayton Smith <argilo@gmail.com>
2019-11-08 21:15:33 -0500

    Add partial solution

--- hash/collide.py
+++ hash/collide.py
@@ -1,15 +1,48 @@
 #!/usr/bin/env python3
 
+import base64
 import hashlib
+import shlex
+import urllib.request
 
-HASH_BYTES = 6
-
-
-def hash(b):
-    return hashlib.sha256(b).digest()[:HASH_BYTES]
+target_command = "ls"
 
 
-x0 = bytes([0] * HASH_BYTES)
+def hash(message):
+    return hashlib.sha256(message).digest()[0:6]
+
+
+def command(text):
+    return f"convert -background lightblue -fill blue -pointsize 40 -size 320x -gravity Center {shlex.quote('caption:' + text)} png:-"
+
+
+seen = {}
+
+for x in range(0x1000000):
+    h = hash(command(str(x)).encode())
+    seen[h] = x
+
+x = 0
+while True:
+    command = f"{target_command} ; echo {x} > /dev/null"
+    h = hash(command.encode())
+    if h in seen:
+        break
+    x += 1
+
+with urllib.request.urlopen(f"http://localhost:5000/?text={seen[h]}") as f:
+    sig = f.read().decode().split('data-signature="')[1].split('"')[0]
+
+data = urllib.parse.urlencode({"command": command, "signature": sig}).encode()
+with urllib.request.urlopen("http://localhost:5000/api/run_command", data) as f:
+    output = base64.b64decode(f.read().split(b",")[1]).decode()
+
+print(output)
+
+exit()
+
+
+x0 = bytes([0] * 6)
 
 tortoise = hash(x0)
 hare = hash(hash(x0))
